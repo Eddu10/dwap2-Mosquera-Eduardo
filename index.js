ae5b593d6080f4979e6e3f119c1be0d179998604
Eddu10 <omarmosquera1998@gmail.com>
2021-08-05 18:49:53 -0500

    subida examen

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 require('./hbs/helper');
+const port=process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,4 +15,4 @@ app.get('/', (req, res)=>{
     res.render('home');
 })
 
-app.listen(8080);
+app.listen(port);
